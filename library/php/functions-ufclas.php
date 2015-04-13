811ad5409379747f7b9f83a8ac070f43f4d8decb
priscillamc <priscillamc@users.noreply.github.com>
2015-04-13 18:55:53 -0400

    Adds styles and widget customization for The Events Calendar

--- library/php/functions-ufclas.php
+++ library/php/functions-ufclas.php
@@ -18,3 +18,23 @@ function ufclas_get_site_socialnetworks() {
 		}
 	}
 }
+
+/**
+ * The Events Calendar functions 
+ */
+function ufclas_events_widget_before_title(){
+	global $post;
+	
+	// Change format of the start date for styling as an icon
+	$tribe_post_month = tribe_get_start_date( $post, false, 'M' );
+	$tribe_post_day = tribe_get_start_date( $post, false, 'j' );
+	?>
+    
+    <div class="event-date">
+    	<span class="event-month"><?php echo $tribe_post_month; ?></span>
+        <span class="event-day"><?php echo $tribe_post_day; ?></span>
+   	</div>
+    
+    <?php 
+}
+add_action( 'tribe_events_list_widget_before_the_event_title', 'ufclas_events_widget_before_title' );
